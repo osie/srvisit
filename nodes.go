0ea86a9c81cb3698d53a2a2608149c141673286d
osiengine <m.heidari@osiengine.com>
2018-08-11 23:08:32 +0430

    Update nodes.go

--- nodes.go
+++ nodes.go
@@ -12,18 +12,18 @@ import (
 )
 
 func masterServer() {
-	logAdd(MESS_INFO, "masterServer запустился")
+	logAdd(MESS_INFO, "masterServer started")
 
 	ln, err := net.Listen("tcp", ":"+options.MasterPort)
 	if err != nil {
-		logAdd(MESS_ERROR, "masterServer не смог занять порт")
+		logAdd(MESS_ERROR, "masterServer could not take port")
 		os.Exit(1)
 	}
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			logAdd(MESS_ERROR, "masterServer не смог занять сокет")
+			logAdd(MESS_ERROR, "masterServer could not take a socket")
 			break
 		}
 
@@ -32,12 +32,12 @@ func masterServer() {
 	}
 
 	ln.Close()
-	logAdd(MESS_INFO, "masterServer остановился")
+	logAdd(MESS_INFO, "masterServer stopped")
 }
 
 func masterHandler(conn *net.Conn) {
 	id := randomString(MAX_LEN_ID_LOG)
-	logAdd(MESS_INFO, id+" masterServer получил соединение")
+	logAdd(MESS_INFO, id+" masterServer received the connection")
 
 	var curNode Node
 
@@ -47,15 +47,15 @@ func masterHandler(conn *net.Conn) {
 		buff, err := reader.ReadBytes('}')
 
 		if err != nil {
-			logAdd(MESS_ERROR, id+" ошибка чтения буфера")
+			logAdd(MESS_ERROR, id+" buffer read error")
 			break
 		}
 
 		logAdd(MESS_DETAIL, id+fmt.Sprint(" buff ("+strconv.Itoa(len(buff))+"): "+string(buff)))
 
-		//удаляем мусор
+		//remove rubbish
 		if buff[0] != '{' {
-			logAdd(MESS_INFO, id+" masterServer удаляем мусор")
+			logAdd(MESS_INFO, id+" masterServer remove garbage")
 			if bytes.Index(buff, []byte("{")) >= 0 {
 				buff = buff[bytes.Index(buff, []byte("{")):]
 			} else {
@@ -66,45 +66,45 @@ func masterHandler(conn *net.Conn) {
 		var message Message
 		err = json.Unmarshal(buff, &message)
 		if err != nil {
-			logAdd(MESS_ERROR, id+" ошибка разбора json")
+			logAdd(MESS_ERROR, id+" parse error json")
 			time.Sleep(time.Millisecond * WAIT_IDLE)
 			continue
 		}
 
 		logAdd(MESS_DETAIL, id+" "+fmt.Sprint(message))
 
-		//обрабатываем полученное сообщение
+		//process the received message
 		if len(processingAgent) > message.TMessage {
 			if processingAgent[message.TMessage].Processing != nil {
-				go processingAgent[message.TMessage].Processing(message, conn, &curNode, id) //от одного агента может много приходить сообщений, не тормозим их
+				go processingAgent[message.TMessage].Processing(message, conn, &curNode, id) //from one agent there can be a lot of messages, do not slow them down
 			} else {
-				logAdd(MESS_INFO, id+" нет обработчика для сообщения")
+				logAdd(MESS_INFO, id+" there is no handler for the message")
 				time.Sleep(time.Millisecond * WAIT_IDLE)
 			}
 		} else {
-			logAdd(MESS_INFO, id+" неизвестное сообщение")
+			logAdd(MESS_INFO, id+" unknown message")
 			time.Sleep(time.Millisecond * WAIT_IDLE)
 		}
 
 	}
 	(*conn).Close()
 
-	//если есть id значит скорее всего есть в карте
+	//if there is id means most likely there is in the map
 	if len(curNode.Id) == 0 {
 		nodes.Delete(curNode.Id)
 	}
 
-	logAdd(MESS_INFO, id+" masterServer потерял соединение с агентом")
+	logAdd(MESS_INFO, id+" masterServer lost connection to agent")
 }
 
 func nodeClient() {
 
-	logAdd(MESS_INFO, "nodeClient запустился")
+	logAdd(MESS_INFO, "nodeClient started")
 
 	for {
 		conn, err := net.Dial("tcp", options.MasterServer+":"+options.MasterPort)
 		if err != nil {
-			logAdd(MESS_ERROR, "nodeClient не смог подключиться: "+fmt.Sprint(err))
+			logAdd(MESS_ERROR, "nodeClient could not connect: "+fmt.Sprint(err))
 			time.Sleep(time.Second * WAIT_IDLE_AGENT)
 			continue
 		}
@@ -124,15 +124,15 @@ func nodeClient() {
 			buff, err := reader.ReadBytes('}')
 
 			if err != nil {
-				logAdd(MESS_ERROR, "nodeClient ошибка чтения буфера: "+fmt.Sprint(err))
+				logAdd(MESS_ERROR, "nodeClient buffer read error: "+fmt.Sprint(err))
 				break
 			}
 
 			logAdd(MESS_DETAIL, fmt.Sprint("buff ("+strconv.Itoa(len(buff))+"): "+string(buff)))
 
-			//удаляем мусор
+			//remove rubbish
 			if buff[0] != '{' {
-				logAdd(MESS_INFO, "nodeClient удаляем мусор")
+				logAdd(MESS_INFO, "nodeClient remove garbage")
 				if bytes.Index(buff, []byte("{")) >= 0 {
 					logAdd(MESS_DETAIL, fmt.Sprint("buff ("+strconv.Itoa(len(buff))+"): "+string(buff)))
 					buff = buff[bytes.Index(buff, []byte("{")):]
@@ -144,56 +144,56 @@ func nodeClient() {
 			var message Message
 			err = json.Unmarshal(buff, &message)
 			if err != nil {
-				logAdd(MESS_ERROR, "nodeClient ошибка разбора json: "+fmt.Sprint(err))
+				logAdd(MESS_ERROR, "nodeClient parse error json: "+fmt.Sprint(err))
 				time.Sleep(time.Millisecond * WAIT_IDLE)
 				continue
 			}
 
 			logAdd(MESS_DETAIL, fmt.Sprint(message))
 
-			//обрабатываем полученное сообщение
+			//process the received message
 			if len(processingAgent) > message.TMessage {
 				if processingAgent[message.TMessage].Processing != nil {
 					go processingAgent[message.TMessage].Processing(message, &conn, nil, randomString(MAX_LEN_ID_LOG))
 				} else {
-					logAdd(MESS_INFO, "nodeClient нет обработчика для сообщения")
+					logAdd(MESS_INFO, "nodeClient no handler for message")
 					time.Sleep(time.Millisecond * WAIT_IDLE)
 				}
 			} else {
-				logAdd(MESS_INFO, "nodeClient неизвестное сообщение")
+				logAdd(MESS_INFO, "nodeClient unknown message")
 				time.Sleep(time.Millisecond * WAIT_IDLE)
 			}
 
 		}
 		conn.Close()
 	}
-	//logAdd(MESS_INFO, "nodeClient остановился") //недостижимо???
+	//logAdd(MESS_INFO, "nodeClient stopped") //unattainable???
 }
 
 func processAgentAuth(message Message, conn *net.Conn, curNode *Node, id string) {
-	logAdd(MESS_INFO, id+" пришла авторизация агента")
+	logAdd(MESS_INFO, id+" the authorization of the agent came")
 
 	if options.Mode == REGULAR {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		(*conn).Close()
 		return
 	}
 
 	if options.Mode == NODE {
-		logAdd(MESS_ERROR, id+" пришел отзыв на авторизацию")
+		logAdd(MESS_ERROR, id+" came feedback on authorization")
 		return
 	}
 
 	time.Sleep(time.Millisecond * WAIT_IDLE)
 
 	if len(message.Messages) != 2 {
-		logAdd(MESS_ERROR, id+" не правильное кол-во полей")
+		logAdd(MESS_ERROR, id+" not the correct number of fields")
 		(*conn).Close()
 		return
 	}
 
 	if message.Messages[1] != options.MasterPassword {
-		logAdd(MESS_ERROR, id+" не правильный пароль")
+		logAdd(MESS_ERROR, id+" wrong password")
 		(*conn).Close()
 		return
 	}
@@ -205,31 +205,31 @@ func processAgentAuth(message Message, conn *net.Conn, curNode *Node, id string)
 
 	if sendMessage(conn, TMESS_AGENT_AUTH, curNode.Id) {
 		nodes.Store(curNode.Id, curNode)
-		logAdd(MESS_INFO, id+" авторизация агента успешна")
+		logAdd(MESS_INFO, id+" agent authorization is successful")
 	}
 }
 
 func processAgentAnswer(message Message, conn *net.Conn, curNode *Node, id string) {
 	if options.Mode != NODE {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		return
 	}
 
-	logAdd(MESS_INFO, id+" пришел ответ на авторизацию агента")
+	logAdd(MESS_INFO, id+" I received a response to the agent's authorization")
 
-	//todo добавить обработку
+	//todo add processing
 }
 
 func processAgentAddCode(message Message, conn *net.Conn, curNode *Node, id string) {
 	if options.Mode != NODE {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		return
 	}
 
-	logAdd(MESS_INFO, id+" пришла информация о создании сессии")
+	logAdd(MESS_INFO, id+" came information about the creation of the session")
 
 	if len(message.Messages) != 1 {
-		logAdd(MESS_ERROR, id+" не правильное кол-во полей")
+		logAdd(MESS_ERROR, id+" not the correct number of fields")
 		return
 	}
 
@@ -238,14 +238,14 @@ func processAgentAddCode(message Message, conn *net.Conn, curNode *Node, id stri
 
 func processAgentDelCode(message Message, conn *net.Conn, curNode *Node, id string) {
 	if options.Mode != NODE {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		return
 	}
 
-	logAdd(MESS_INFO, id+" пришла информация об удалении сессии")
+	logAdd(MESS_INFO, id+" information about deleting a session")
 
 	if len(message.Messages) != 1 {
-		logAdd(MESS_ERROR, id+" не правильное кол-во полей")
+		logAdd(MESS_ERROR, id+" not the correct number of fields")
 		return
 	}
 
@@ -254,14 +254,14 @@ func processAgentDelCode(message Message, conn *net.Conn, curNode *Node, id stri
 
 func processAgentNewConnect(message Message, conn *net.Conn, curNode *Node, id string) {
 	if options.Mode != MASTER {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		return
 	}
 
-	logAdd(MESS_INFO, id+" пришла информация о новом соединения")
+	logAdd(MESS_INFO, id+" information about a new connection")
 
 	//if len(message.Messages) != 1 {
-	//	logAdd(MESS_ERROR, id + " не правильное кол-во полей")
+	//	logAdd(MESS_ERROR, id + " not the correct number of fields")
 	//	return
 	//}
 	//
@@ -269,7 +269,7 @@ func processAgentNewConnect(message Message, conn *net.Conn, curNode *Node, id s
 	//
 	//value, exist := channels.Load(code)
 	//if exist == false {
-	//	logAdd(MESS_ERROR, id + " не ждем такого соединения " + code)
+	//	logAdd(MESS_ERROR, id + " do not wait for such a connection " + code)
 	//	disconnectPeers(code)
 	//	return
 	//}
@@ -283,40 +283,40 @@ func processAgentNewConnect(message Message, conn *net.Conn, curNode *Node, id s
 	//}
 	//peers.mutex.Unlock()
 	//
-	////мы должны дождаться два соединения
+	////we must wait for two connections
 	//var cWait = 0
 	//for (peers.node[0] == nil || peers.node[1] == nil) && cWait < WAIT_COUNT {
-	//	logAdd(MESS_INFO, id + " ожидаем пира для " + code)
+	//	logAdd(MESS_INFO, id + " we are waiting for a feast for " + code)
 	//	time.Sleep(time.Millisecond * WAIT_IDLE)
 	//	cWait++
 	//}
 	//
-	////если не дождались одного из пира
+	////if you did not wait for one of the feasts
 	//for peers.node[0] == nil || peers.node[1] == nil {
-	//	logAdd(MESS_ERROR, id + " не дождались пира для " + code)
+	//	logAdd(MESS_ERROR, id + " did not wait for a feast for " + code)
 	//	disconnectPeers(code)
 	//	return
 	//}
 	//
-	////если они у одного агента, то ничего
+	////if they are from the same agent, then nothing
 	//if peers.node[0].Id == peers.node[1].Id {
-	//	logAdd(MESS_INFO, id + " пиры у одного агента " + code)
+	//	logAdd(MESS_INFO, id + " feasts from one agent " + code)
 	//	return
 	//}
 	//
-	//logAdd(MESS_INFO, id + " отправили запрос на соединение агента к агенту " + code)
+	//logAdd(MESS_INFO, id + " sent a request to connect the agent to the agent " + code)
 }
 
 func processAgentDelConnect(message Message, conn *net.Conn, curNode *Node, id string) {
 	if options.Mode != MASTER {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		return
 	}
 
-	logAdd(MESS_INFO, id+" пришла информация об удалении соединения")
+	logAdd(MESS_INFO, id+" information about the connection removal")
 
 	if len(message.Messages) != 1 {
-		logAdd(MESS_ERROR, id+" не правильное кол-во полей")
+		logAdd(MESS_ERROR, id+" not the correct number of fields")
 		return
 	}
 
@@ -325,14 +325,14 @@ func processAgentDelConnect(message Message, conn *net.Conn, curNode *Node, id s
 
 func processAgentAddBytes(message Message, conn *net.Conn, curNode *Node, id string) {
 	if options.Mode != MASTER {
-		logAdd(MESS_ERROR, id+" режим не поддерживающий агентов")
+		logAdd(MESS_ERROR, id+" non-agent mode")
 		return
 	}
 
-	logAdd(MESS_INFO, id+" пришла информация статистики")
+	logAdd(MESS_INFO, id+" came information statistics")
 
 	if len(message.Messages) != 1 {
-		logAdd(MESS_ERROR, id+" не правильное кол-во полей")
+		logAdd(MESS_ERROR, id+" not the correct number of fields")
 		return
 	}
 
